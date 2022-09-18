97c2164d7977ae79e257d320367d392e534f167b
github_robin <robin.schmitz2@gmx.de>
2022-09-18 12:55:49 +0200

    add last comments

--- test/DappToken.js
+++ test/DappToken.js
@@ -4,118 +4,115 @@ var DappToken = artifacts.require("DappToken");
 
 contract("DappToken", function(accounts){
 	var tokenInstance;
+	var wantedBalance = 1000000;
 
-	it("initializes the contract with the correct values", function(){
+	it("1. Test: Wird der SC mit den richtigen Werten initialisiert?", function(){ // Prüfung: Entspricht der Name und das Symbol unseren Vorgaben?
 		return DappToken.deployed().then(function(instance){
 			tokenInstance = instance;
 			return tokenInstance.name();
 		}).then(function(name){
-			assert.equal(name, "Token", "has the correct name");
+			assert.equal(name, "Token", "... hat nicht den richtigen Namen");
 		return tokenInstance.symbol();
 		}).then(function (symbol) {
-			assert.equal(symbol, "FOM", "has the correct symbol");
-			return tokenInstance.standard();
-		}).then(function(standard){
-			assert.equal(standard, "Dapp Token V1.0", "has the correct standard");
+			assert.equal(symbol, "FOM", "... hat nicht das richtige Symbol");
 		});
 	})
 
 
-	it("sets the total supply upon deployment", function(){
+	it("2. Test: Wird das Guthaben in richtiger Höhe verteilt?", function(){ // Prüfung: Entspricht die Guthaben-Verteilung unseren Vorgaben?
 		return DappToken.deployed().then(function (instance) {
 			tokenInstance = instance;
 			return tokenInstance.totalSupply();
 		}).then(function(totalSupply){
-			assert.equal(totalSupply.toNumber(), 1000000, "sets the total supply to 1,000,000");
+			assert.equal(totalSupply.toNumber(), wantedBalance, "die totalSupply-Variable entspricht nicht den Vorgaben");
 			return tokenInstance.balanceOf(accounts[0]);
 		}).then(function (adminBalance) {
-			assert.equal(adminBalance.toNumber(), 1000000, "it allocates the initial supply to the admin acc")
+			assert.equal(adminBalance.toNumber(), wantedBalance, "das Guthaben des Admin-Kontos entspricht nicht der totalSupply-Variable")
 		});
 	});
 
 
-	it("transfers token ownership", function(){
+it("3. Test: Wird ein Transfer ordnungsgemäß durchgeführt?", function(){ // Prüfung: hier werden verschiedene Tests rund um den Token-Transfer ausgeführt.
 		return DappToken.deployed().then(function(instance){
 			tokenInstance = instance;
-			return tokenInstance.transfer.call(accounts[1], 999999999999); // call does not trigger transaction -> gets the return value
+			return tokenInstance.transfer.call(accounts[1], 999999999999); 
 		}).then(assert.fail).catch(function(error){
-			assert(error.message.toString().indexOf("revert") >=0, "error message must contain revert");
+			assert(error.message.toString().indexOf("revert") >=0, "fehlerhafte Übermittlung wird nicht abgefangen");
 			return tokenInstance.transfer.call(accounts[1], 250000, {from: accounts[0]});
 		}).then(function(success){
-			assert.equal(success, true, "it returns true");
-			return tokenInstance.transfer(accounts[1], 250000, {from: accounts[0]});
-		}).then(function(receipt){
-			 assert.equal(receipt.logs.length, 1, 'triggers one event');
-			 assert.equal(receipt.logs[0].event, 'Transfer', 'should be the "Transfer" event');
-			 assert.equal(receipt.logs[0].args._from, accounts[0], 'logs the account the tokens are transferred from');
-			 assert.equal(receipt.logs[0].args._to, accounts[1], 'logs the account the tokens are transferred to');
-			 assert.equal(receipt.logs[0].args._value, 250000, 'logs the transfer amount');
+			assert.equal(success, true, "es wird kein true-Wert zurückgebenen; der Transfer war somit nicht erfolgreich");
+			return tokenInstance.transfer(accounts[1], 250000, {from: accounts[0]}); // Testung eines Transfers i. H. v. 25.000 Token
+		}).then(function(receipt){ // das receipt entsteht nach erfolgreich durchgeführtem Transfer und enthält verschiedene Informationen
+			 assert.equal(receipt.logs.length, 1, 'es sollte nur ein Event getriggert werden');
+			 assert.equal(receipt.logs[0].event, 'Transfer', 'es sollte das "Transfer"-Event getriggert werden');
+			 assert.equal(receipt.logs[0].args._from, accounts[0], 'der Transfer-From Account stimmt nicht überein');
+			 assert.equal(receipt.logs[0].args._to, accounts[1], 'der Transfer-To Account stimmt nicht überein');
+			 assert.equal(receipt.logs[0].args._value, 250000, 'die Transfer-Summe stimmt nicht überein');
 			return tokenInstance.balanceOf(accounts[1]);
-		}).then(function(balance){
-			assert.equal(balance.toNumber(), 250000, "adds the amount to the receiving accounts");
+		}).then(function(balance){	// überprüft das Guthaben nach Durchführung des Transfers - Empfänger
+			assert.equal(balance.toNumber(), 250000, "Summe stimmt nicht mit Transfer-Summe überein - Empfänger");
 			return tokenInstance.balanceOf(accounts[0]);
-		}).then(function(balance){
-			assert.equal(balance.toNumber(), 750000, "deducts the amount from the sending accounts");
+		}).then(function(balance){	// überprüft das Guthaben nach Durchführung des Transfers - Sender
+			assert.equal(balance.toNumber(), 750000, "Summe stimmt nicht mit Transfer-Summe überein - Sender");
 		});
 	});
 
 
-	it("approves tokens for delegated transfer", function(){
+	it("4. Test: Wird die approve-Funktion ordnungsgemäß ausgeführt?", function(){ // Prüfung: hier werden Tests rund um die approve-Funktion durchgeführt
 		return DappToken.deployed().then(function (instance) {
 			tokenInstance = instance;
-			return tokenInstance.approve.call(accounts[1], 100); // Call triggers function without creating transaction on the blockchain
+			return tokenInstance.approve.call(accounts[1], 100); 
 		}).then(function (success) {
-			assert.equal(success, true, "it returns true");
-			return tokenInstance.approve(accounts[1], 100, {from: accounts[0]}); // allow account[1] to spend 100 Tokens on our behalf (account[0])
+			assert.equal(success, true, "es wird kein true-Wert zurückgebenen; aprrove war somit nicht erfolgreich");
+			return tokenInstance.approve(accounts[1], 100, {from: accounts[0]}); // erlaubt account[1] 100 Tokens von account[0] auszugeben
 		}).then(function(receipt){
-			  assert.equal(receipt.logs.length, 1, 'triggers one event');
-			  assert.equal(receipt.logs[0].event, 'Approval', 'should be the "Approval" event');
-			  assert.equal(receipt.logs[0].args._owner, accounts[0], 'logs the account the tokens are authorized by');
-			  assert.equal(receipt.logs[0].args._spender, accounts[1], 'logs the account the tokens are authorized to');
-			  assert.equal(receipt.logs[0].args._value, 100, 'logs the transfer amount');
+			  assert.equal(receipt.logs.length, 1, 'es sollte nur ein Event getriggert werden');
+			  assert.equal(receipt.logs[0].event, 'Approval', 'es sollte das "Approval"-Event getriggert werden');
+			  assert.equal(receipt.logs[0].args._owner, accounts[0], 'der Approval-From Account stimmt nicht überein');
+			  assert.equal(receipt.logs[0].args._spender, accounts[1], 'der Approval-To Account stimmt nicht überein');
+			  assert.equal(receipt.logs[0].args._value, 100, 'die Approval-Summe stimmt nicht überein');
 			  return tokenInstance.allowance(accounts[0], accounts[1]);
 		}).then(function(allowance){
-			assert.equal(allowance.toNumber(), 100, "Stores the allowance for delegated transfer");
+			assert.equal(allowance.toNumber(), 100, "die Allowance-Summe von _from zu _to stimmt nicht überein");
 		});
 	});
 
 
-	it("handels delegated transfer", function(){
-		return DappToken.deployed().then(function (instance) {
+	it("5. Test: Wird die transferFrom-Funktion ordnungsgemäß ausgeführt?", function(){ // Prüfung: hier werden Tests rund um die transferFrom-Funktion durchgeführt
+		return DappToken.deployed().then(function (instance) { // Initialisierung des Tokens und der drei Beteiligten Accounts
 			tokenInstance = instance;
 			fromAccount = accounts[2];
 			toAccount = accounts[3];
 			spendingAccount = accounts[4];
-			// transfer some tokens to [4] -> so he has a balance that he can delegated to transfer
-			return tokenInstance.transfer(fromAccount, 100, {from: accounts[0]});
+			return tokenInstance.transfer(fromAccount, 100, {from: accounts[0]}); // Voraussetzung, dass Tokens transferiert werden können, ist das der fromAccount über das entsprechende Guthaben verfügt -> wird hier transferiert
 		}).then(function(receipt){
-			return tokenInstance.approve(spendingAccount, 10, {from: fromAccount}); //approve spending account
+			return tokenInstance.approve(spendingAccount, 10, {from: fromAccount}); //approve des spending-Accounts
 		}).then(function(receipt){
-			return tokenInstance.transferFrom(fromAccount, toAccount, 999, {from: spendingAccount}); // test if balance is <= transfer amount
+			return tokenInstance.transferFrom(fromAccount, toAccount, 999, {from: spendingAccount}); // Test: transferring-Summe > verfügbare Summe
 		}).then(assert.fail).catch(function(error){
-			assert(error.message.toString().indexOf("revert")>=0, "cannot transfer value lager than balance");
-			return tokenInstance.transferFrom(fromAccount, toAccount, 20, {from: spendingAccount}); // test transferring > sum than approved amount
+			assert(error.message.toString().indexOf("revert")>=0, "Transfer zurückgewiesen: Es kann keine größere Summe als das Guthaben transferiert werden");
+			return tokenInstance.transferFrom(fromAccount, toAccount, 20, {from: spendingAccount}); // Test: transferring-Summe > freigegebene Summe
 		}).then(assert.fail).catch(function(error){
-			assert(error.message.toString().indexOf("revert")>=0, "cannot transfer value lager than approved amount");
-			return tokenInstance.transferFrom.call(fromAccount, toAccount, 10, {from: spendingAccount}); // test return value - valid amount of tokens + call function
+			assert(error.message.toString().indexOf("revert")>=0, "Transfer zurückgewiesen: Es kann keine größere Summe als die freigegebene Summer transferiert werden");
+			return tokenInstance.transferFrom.call(fromAccount, toAccount, 10, {from: spendingAccount}); 
 		}).then(function(success){
-			assert.equal(success, true);
-			return tokenInstance.transferFrom(fromAccount, toAccount, 10, {from: spendingAccount}); // test transfer event - unlike the other test, this one goes through and creates a transaction on the blockchain
+			assert.equal(success, true, "es wird kein true-Wert zurückgebenen; der delegated-Transfer war somit nicht erfolgreich");
+			return tokenInstance.transferFrom(fromAccount, toAccount, 10, {from: spendingAccount}); // Dieser Test ist valide und sollte durchgehen -> es wird ein Transfer-Event erzeugt
 		}).then(function(receipt){
-			assert.equal(receipt.logs.length, 1, 'triggers one event');
-			assert.equal(receipt.logs[0].event, 'Transfer', 'should be the "Transfer" event');
-			assert.equal(receipt.logs[0].args._from, fromAccount, 'logs the account the tokens are transferred from');
+			assert.equal(receipt.logs.length, 1, 'es sollte nur ein Event getriggert werden');
+			assert.equal(receipt.logs[0].event, 'Transfer', 'es sollte das "Transfer"-Event getriggert werden');
+			assert.equal(receipt.logs[0].args._from, fromAccount, 'der Transfer-From Account stimmt nicht überein');
 			assert.equal(receipt.logs[0].args._to, toAccount, 'logs the account the tokens are transferred to');
-			assert.equal(receipt.logs[0].args._value, 10, 'logs the transfer amount');
+			assert.equal(receipt.logs[0].args._value, 10, 'die Transfer-Summe stimmt nicht überein');
 			return tokenInstance.balanceOf(fromAccount);
 		}).then(function(balance){
-			assert.equal(balance.toNumber(), 90, "deducts the amount from the sending account");
+			assert.equal(balance.toNumber(), 90, "Summe stimmt nicht mit Transfer-Summe überein - Sender");
 			return tokenInstance.balanceOf(toAccount);
 		}).then(function(balance){
-			assert.equal(balance.toNumber(), 10, "adds the amount from the receiving account");
+			assert.equal(balance.toNumber(), 10, "Summe stimmt nicht mit Transfer-Summe überein - Empfänger");
 			return tokenInstance.allowance(fromAccount, spendingAccount);
 		}).then(function(allowance){
-			assert.equal(allowance.toNumber(), 0, "deducts the amount from the allowance");
+			assert.equal(allowance.toNumber(), 0, "Summe stimmt nicht mit allowance-Summe überein");
 		});
 	});	
 })
--- test/TokenSale.js
+++ test/TokenSale.js
@@ -6,79 +6,73 @@ contract("TokenSale", function(accounts){
 	var tokenInstance;
 	var admin = accounts[0];
 	var buyer = accounts[1];
-	var tokenPrice = 1000000000000000; //in wei -> smallest unit of Ether -> 0,001 Ether - there no floats, like 1 € = 100 Cents
-	var tokensAvailable = 750000;
+	var tokenPrice = 1000000000000000; 	//in wei -> kleinste Ether-Einheit entspricht 0,001 Ether - es gibt keine Gleitkommazahlen; vergleichbar wenn wir alles in Cent rechnen würden, like 1 € = 100 Cents
+	var tokensAvailable = 750000;		// Anzahl an Token, die für den Sale verfügbar sind (von 100.000)
 	var numberOfTokens;
 
-	it('initializes the contract with the correct values', function() {
+	it('1. Test: Wird der SC mit den richtigen Werten initialisiert?', function() {
 	    return TokenSale.deployed().then(function(instance) {
 	      tokenSaleInstance = instance;
 	      return tokenSaleInstance.address
 	    }).then(function(address) {
-	      assert.notEqual(address, 0x0, 'has contract address');
+	      assert.notEqual(address, 0x0, '... der TokenSale-SC hat keine Adresse');
 	      return tokenSaleInstance.tokenContract();
 	    }).then(function(address) {
-	      assert.notEqual(address, 0x0, 'has token contract address');
+	      assert.notEqual(address, 0x0, '... der Token-SC hat keine Adresse');
 	      return tokenSaleInstance.tokenPrice();
 	    }).then(function(price) {
-	      assert.equal(price, tokenPrice, 'token price is correct');
+	      assert.equal(price, tokenPrice, '... der Token-Preis ist nicht korrekt');
 	    });
 	});
 
-	it("facilitates token buyin", function(){
+	it("2. Test: Wird der Token-Kauf erfolgreich abgewickelt?", function(){
 		return DappToken.deployed().then(function(instance){
-			// grab tokeninstance first
-			tokenInstance = instance;
+			tokenInstance = instance;		// zuerst wird der Token instanziiert - die Reihenfolge ist wichtig.
 			return TokenSale.deployed();
 		}).then(function(instance){
-			// after that grab tokensale instance
-			tokenSaleInstance = instance;
-			// Provision 75 % of tokens to sale
-			return tokenInstance.transfer(tokenSaleInstance.address, tokensAvailable, {from: admin})
+			tokenSaleInstance = instance;	// danach wird TokenSale instanziiert
+			return tokenInstance.transfer(tokenSaleInstance.address, tokensAvailable, {from: admin}) // Bereitstellung von 75 % der Gesamtsumme
 		}).then(function(receipt){
 			numberOfTokens = 10;
-			return tokenSaleInstance.buyTokens(numberOfTokens, {from: buyer, value: numberOfTokens * tokenPrice}) // value = amount of wei that is needed to pay
+			return tokenSaleInstance.buyTokens(numberOfTokens, {from: buyer, value: numberOfTokens * tokenPrice}) // value = Summe von wei
 		}).then(function(receipt){
-			 assert.equal(receipt.logs.length, 1, 'triggers one event');
-			 assert.equal(receipt.logs[0].event, 'Sell', 'should be the "Sell" event');
-			 assert.equal(receipt.logs[0].args._buyer, buyer, 'logs the account that purchased the tokens');
-			 assert.equal(receipt.logs[0].args._amount, numberOfTokens, 'logs the number of tokens purchased');
+			 assert.equal(receipt.logs.length, 1, 'es sollte nur ein Event getriggert werden');
+			 assert.equal(receipt.logs[0].event, 'Sell', 'es sollte das "Sell"-Event getriggert werden');
+			 assert.equal(receipt.logs[0].args._buyer, buyer, 'der Käufer-Account stimmt nicht überein');
+			 assert.equal(receipt.logs[0].args._amount, numberOfTokens, 'die Anzal der gekauften Token stimmt nicht überein');
 			return tokenSaleInstance.tokensSold();
 		}).then(function (amount) {
-			assert.equal(amount.toNumber(), numberOfTokens, "increments the number of tokens sold");
+			assert.equal(amount.toNumber(), numberOfTokens, "... die Nummer der verkauften Tokens muss entsprechend erhöht werden");
 			return tokenInstance.balanceOf(buyer);
 		}).then(function (balance) {
 			assert.equal(balance.toNumber(), numberOfTokens);
 			return tokenInstance.balanceOf(tokenSaleInstance.address);
 		}).then(function (balance) {
-			assert.equal(balance.toNumber(), tokensAvailable - numberOfTokens, "balance has to equal tokens left");
-			return tokenSaleInstance.buyTokens(numberOfTokens, {from: buyer, value: 1}); //tries to buy Tokens on a way to low wei-value - catched by require(msg.value == multiply(_numberOfTokens, tokenPrice));
+			assert.equal(balance.toNumber(), tokensAvailable - numberOfTokens, "das neue Guthaben muss mit den übrigen Token übereinstimmen");
+			return tokenSaleInstance.buyTokens(numberOfTokens, {from: buyer, value: 1}); //versucht zu niedrig (wei-value) zu kaufen 
 		}).then(assert.fail).catch(function(error){
-			assert(error.message.toString().indexOf("revert") >=0, "msg.value must equal number of tokens * wei");
-			return tokenSaleInstance.buyTokens(numberOfTokens+ 1, {from: buyer, value: numberOfTokens * tokenPrice}); // tries to buy more tokens than available - catched by require(tokenContract.transfer((msg.sender), _numberOfTokens));
+			assert(error.message.toString().indexOf("revert") >=0, "msg.value muss tokens * wei entsprechen");
+			return tokenSaleInstance.buyTokens(numberOfTokens+ 1, {from: buyer, value: numberOfTokens * tokenPrice}); // versucht mehr Token als verfügbar zu kaufen
 		}).then(assert.fail).catch(function(error){
-			assert(error.message.toString().indexOf("revert") >=0, "cannot purchase more tokens than available"); 
+			assert(error.message.toString().indexOf("revert") >=0, "es können nich mehr Token als verfügbar gekauft werden"); 
 		});
 	});
 
-	it("ends token sale", function(){
+	it("3. Test: Wird der Token-Sale ordnungsgemäß beendet?", function(){
 		return DappToken.deployed().then(function(instance){
-			// grab tokeninstance first
 			tokenInstance = instance;
 			return TokenSale.deployed();
 		}).then(function(instance){
-			// after that grab tokensale instance
 			tokenSaleInstance = instance;
-			//try end sale from accounts != admin
-			return tokenSaleInstance.endSale({from: buyer});
+			return tokenSaleInstance.endSale({from: buyer}); //versucht Token-Sale von Account != admin zu beenden
 		}).then(assert.fail).catch(function (error) {
-			assert(error.message.toString().indexOf("revert") >=0, "must be admin to end sale");
+			assert(error.message.toString().indexOf("revert") >=0, "die Beendigung kann nur durch den Admin durchgeführt werden");
 			// end sale as admin
 			return tokenSaleInstance.endSale({from: admin});
 		}).then(function(receipt){
 			return tokenInstance.balanceOf(admin);
 		}).then(function(balance){
-			assert.equal(balance.toNumber(), 999990, "returns all unsold dapp token to admin"); // 10 tokens have been transfered, so 999990 have to go back
+			assert.equal(balance.toNumber(), 999990, "Summe der zurücktransferierten Tokens stimmt nicht überein"); // 10 sind oben transferiert worden, also müssen 999990 zurück gehen
 		})
 	});
 });
